8fc936c2f21247d8fb61e13e7b22351ef9f925f0
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-09-26 11:28:16 +0600

    Add some section

--- src/main.js
+++ src/main.js
@@ -6,9 +6,20 @@ btn.addEventListener("click", () => {
 	menu.classList.toggle("hidden");
 });
 
-// Close dropdown if clicked outside
-window.addEventListener("click", (e) => {
-	if (!btn.contains(e.target) && !menu.contains(e.target)) {
-		menu.classList.add("hidden");
-	}
+// window.addEventListener("click", (e) => {
+// 	if (!btn.contains(e.target) && !menu.contains(e.target)) {
+// 		menu.classList.add("hidden");
+// 	}
+// });
+
+const Btn = document.getElementById("dayBtn");
+const Menu = document.getElementById("dayMenu");
+
+Btn.addEventListener("click", () => {
+	Menu.classList.toggle("hidden");
 });
+
+const errorSection = document.getElementById("errorSection");
+
+errorSection.classList.remove("hidden");
+errorSection.classList.add("hidden");
